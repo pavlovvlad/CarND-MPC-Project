1b3c068ea3e1cb942f9371ff390c497831d448d6
vpavlov <vladislav.m.pavlov@googlemail.com>
2017-10-14 22:35:45 +0200

    increase the scale factor for steering angle in cost function to 1000

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -57,7 +57,7 @@ class FG_eval {
     double cte_scale = 3000.0;
     double epsi_scale = 1000.0;
     double a_scale = 1.0;
-    double delta_scale = 1.0;
+    double delta_scale = 1000.0;
     double a1_scale = 1.0;
     double delta1_scale = 1000.0;
 
